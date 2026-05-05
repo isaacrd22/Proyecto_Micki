011387f1315635c8b1589252ac727c90e9143322
isaacrd22 <diemaro1716@gmail.com>
2026-05-05 12:24:51 -0600

    v2

---
+++ src/main/java/peneDurbat.java
@@ -0,0 +1,9 @@
+public class peneDurbat {
+    private short altura;
+    private String color;
+
+    public peneDurbat(short altura, String color) {
+        this.altura = altura;
+        this.color = color;
+    }
+}
